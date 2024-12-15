b2ac5940da1b2673dd23afc5f272c2805c8f2598
Ken Moini <ken@kenmoini.com>
2024-12-15 06:31:23 +0000

    logic fix

--- pika-pki.sh
+++ pika-pki.sh
@@ -14,6 +14,8 @@ if [ -d "${SCRIPT_DIR}/overrides" ]; then
   else
     source ${SCRIPT_DIR}/functions/config.sh
   fi
+else
+  source ${SCRIPT_DIR}/functions/config.sh
 fi
 source ${SCRIPT_DIR}/functions/root-ca.sh
 source ${SCRIPT_DIR}/functions/intermediate-ca.sh

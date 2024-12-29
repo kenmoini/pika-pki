ac6e9304bc729dc40f148d812ea14d60cdcf97df
Ken Moini <ken@kenmoini.com>
2024-12-29 14:54:17 -0500

    add defaults, better overrides, strip slashes, confirm state and output

--- pika-pki.sh
+++ pika-pki.sh
@@ -9,16 +9,17 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 #==============================================================================
 # Source other files
 #==============================================================================
+source ${SCRIPT_DIR}/functions/defaults.sh
 source ${SCRIPT_DIR}/functions/common.sh
 source ${SCRIPT_DIR}/functions/formatting.sh
+source ${SCRIPT_DIR}/functions/config.sh
 if [ -d "${SCRIPT_DIR}/overrides" ]; then
+  if [ -f "${SCRIPT_DIR}/overrides/defaults.sh" ]; then
+    source ${SCRIPT_DIR}/overrides/defaults.sh
+  fi
   if [ -f "${SCRIPT_DIR}/overrides/config.sh" ]; then
     source ${SCRIPT_DIR}/overrides/config.sh
-  else
-    source ${SCRIPT_DIR}/functions/config.sh
   fi
-else
-  source ${SCRIPT_DIR}/functions/config.sh
 fi
 source ${SCRIPT_DIR}/functions/root-ca.sh
 source ${SCRIPT_DIR}/functions/intermediate-ca.sh
@@ -36,8 +37,8 @@ export PIKA_PKI_DEFAULT_STATE=${PIKA_PKI_DEFAULT_STATE:=""}
 export PIKA_PKI_DEFAULT_LOCALITY=${PIKA_PKI_DEFAULT_LOCALITY:=""}
 export PIKA_PKI_DEFAULT_EMAIL=${PIKA_PKI_DEFAULT_EMAIL:=""}
 export PIKA_PKI_CERT_KEY_ENCRYPTION=${PIKA_PKI_CERT_KEY_ENCRYPTION:="false"}
-export PIKA_PKI_DEFAULT_CRL_URI_BASE=${PIKA_PKI_DEFAULT_CRL_URI_BASE:=""}
-PIKA_PKI_DEFAULT_CRL_URI_BASE=$(stripLS ${PIKA_PKI_DEFAULT_CRL_URI_BASE})
+export PIKA_PKI_DEFAULT_CA_URI_BASE=${PIKA_PKI_DEFAULT_CA_URI_BASE:=""}
+PIKA_PKI_DEFAULT_CA_URI_BASE=$(stripLastSlash ${PIKA_PKI_DEFAULT_CA_URI_BASE})
 
 
 export PIKA_PKI_DIR=${PIKA_PKI_DIR:="$(pwd)/.pika-pki"}
@@ -48,7 +49,12 @@ if [ $# -eq 0 ]; then
   #==============================================================================
   echo "===== Working PKI Base Directory: ${PIKA_PKI_DIR}"
   echo "Do you want to continue with this directory?"
-  gum confirm && echo -e "- Continuing...\n" || exit 1
+  if gum confirm; then
+    echo -e "- Continuing...\n"
+  else
+    echo "Exiting..."
+    exit 1
+  fi
 
   mkdir -p ${PIKA_PKI_DIR}/{roots,private_bundles,public_bundles/{certs,crls}}
 
